a1749c2dad3356cf3f8452f99b1657f95be99422
surjayanjati <surjojati@gmail.com>
2023-03-04 01:03:05 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,19 @@
+/// Requiring The Module/Pakages -------------------------------------------------------------------------------->
+const express=require("express");
+
+
+
+
+/// Using The Express ------------------------------------------------------------------------------------------->
+const app=express();
+
+
+
+
+
+
+
+/// Listening To The Port Number -------------------------------------------------------------------------------->
+app.listen(7890,()=>{
+    console.log("Listening To The Port Number 7890");
+})
